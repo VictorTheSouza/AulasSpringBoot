599f5c428637875e95cb700cb2055387f1ca7795
Victor de Souza <83593281+VictorTheSouza@users.noreply.github.com>
2021-06-02 21:44:06 -0300

    Add files via upload

---
+++ demo/src/main/java/org/generation/Farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package org.generation.Farmacia.repository;
+
+import java.util.List;
+
+import org.generation.Farmacia.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List <Produto> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
+
+}
+
